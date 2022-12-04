3290f5038d8d13bb456181e9eff74902b3d2c826
Serhii Kaushnian <skaushnjan@gmail.com>
2022-12-04 12:51:22 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -6,8 +6,12 @@ const updateBooks = async (books) => {
   fs.writeFile(contactsPath, JSON.stringify(books, null, 2));
 };
 const getAllContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 const addContact = async ({ name, email, phone }) => {
   const newBook = {
@@ -16,25 +20,39 @@ const addContact = async ({ name, email, phone }) => {
     email,
     phone,
   };
-  const contacts = await getAllContacts();
-  contacts.push(newBook);
-  await updateBooks(contacts);
-  return newBook;
-};
-const getContactById = async (id) => {
-  const contacts = await getAllContacts();
-  const oneContact = contacts.find((contact) => contact.id === id);
-  return oneContact || null;
-};
-const removeContactById = async (id) => {
-  const contacts = await getAllContacts();
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) {
-    return null;
+  try {
+    const contacts = await getAllContacts();
+    contacts.push(newBook);
+    await updateBooks(contacts);
+    return newBook;
+  } catch (error) {
+    console.log(error);
   }
-  const [result] = contacts.splice(index, 1);
-  await updateBooks(contacts);
-  return result;
+};
+
+const getContactById = async (id) => {
+  try {
+    const contacts = await getAllContacts();
+    const oneContact = contacts.find((contact) => contact.id === id);
+    return oneContact || null;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContactById = async (id) => {
+  try {
+    const contacts = await getAllContacts();
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateBooks(contacts);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 };
 module.exports = {
   getAllContacts,
